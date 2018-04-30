039a0ace5646bda02915969e5c1c3b08fc09e50e
John <cooks.johns@gmail.com>
2018-04-30 11:53:44 -0500

    problem 5

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,28 @@
+/**
+ * ClassName needs to match the filename
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
 
+        //----==============   notes
+
+        System.out.println(); // An empty sout gives you a new line
+        System.out.println("The shortcut to make a main method is psvm followed by tab..");
+        System.out.println("The shortcut for System.out.println() is sout then a tab...");
+
+        int myFavoriteNumber = 30;
+        String myString = "My favorite number is ";
+
+        System.out.println(myString + myFavoriteNumber + "!");
+
+        long myNumber = ;
+
+
+
+
+
+
+
+    }
 }
