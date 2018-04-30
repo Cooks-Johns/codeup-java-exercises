c64d27421aff1bc6b59f4775bad8b62c74a9d97e
John <cooks.johns@gmail.com>
2018-04-30 14:01:19 -0500

    problem 3

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
+    public static void main(String[] args){
+        String userInput;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Show Me What You Got!  ");
+        userInput =  sc.nextLine();
+
+
+
+        System.out.println("Is that all You got? " + userInput + " ,HAHAHAHAHAHA");
+        System.out.println("==================");
+        sc.nextLine();
+
+
+        int facvNum;
+        System.out.println("givwe it to me");
+        facvNum = sc.nextInt();
+
+        System.out.println("your num" + facvNum);
+
+    }
 }
---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,18 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+
+
+        int i = 5;
+        while (i < 16) {
+            System.out.println(i);
+            i++;
+        }
+
+        int doWhile = 0;
+        do{
+            System.out.println(doWhile);
+            doWhile +=2 ;
+        } while (doWhile <= 100);
+
+    }
+}
