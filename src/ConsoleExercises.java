fa310fcc8fe5fced4289d5b2a20fb5a1bfff83b7
John <cooks.johns@gmail.com>
2018-05-02 19:03:22 -0500

    fixed helloWorld

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+//        Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
+
+//
+
+        System.out.print("Enter a number: ");
+        int usrInt = sc.nextInt();
+
+//        Prompt a user to enter 3 words and store each of them in a separate variable, then display them back, each on a newline.
+//        What happens if you enter less than 3 words?
+//        What happens if you enter more than 3 words?
+
+        System.out.print("Please enter three words: " );
+        String firstWord = sc.next();
+        String secondWord = sc.next();
+        String thirdWord = sc.next();
+
+
+        System.out.println("All the text entered: " + firstWord + " " + secondWord + " " + thirdWord);
+
+        sc.nextLine();  // grabs the last \n charcter and we can ignore it
+
+
+//        Prompt a user to enter a sentence, then store that sentence in a String variable using the .next method, then display that sentence back to the user.
+//        do you capture all of the words?
+
+        System.out.print("Please enter a sentence: " );
+        String thisSentence = sc.nextLine(); //have to use nextLine() for a sentence
+        System.out.println("All the text entered: " + thisSentence);
+
+//        Calculate the perimeter and area of Codeup's classrooms
+//        Prompt the user to enter values of length and width of the classroom.
+//        Use the .nextLine method to get user input and cast the resulting string to a numeric type.
+//          Assume that the rooms are perfect rectangles.
+//          Assume that the user will enter valid numeric data for length and width.
+//          Display the area and perimeter of that classroom.
+
+        System.out.println();
+        System.out.print("Input the length: ");
+        String lengthString = sc.next();
+        double length = Double.parseDouble(lengthString);
+        System.out.println("length is " + length);
+
+
+//              area = length x width
+//              perimter = 2 x length + 2 x width
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the length of Rectangle:");
+//        double length = sc.nextDouble();
+//
+//        System.out.println("Enter the width of Rectangle:");
+//        double width = sc.nextDouble();
+//
+//        System.out.println("Enter the height of Rectangle:");
+//        double height = sc.nextDouble();
+//
+//        double area = length*width;
+//        double volume =length*width*height;
+//        System.out.println("Area of Rectangle is: " + area + " the volume of the Rectangle is: " + volume);
+
+        }
+    }
+
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,10 @@
 public class HelloWorld {
-    pusblic
+
+    public static void main(String[] args) {
+        System.out.println("Hello, CodeUp!");
+    }
+    public static void characterList(){
+
+    }
+
 }
