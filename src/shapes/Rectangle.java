e36daa58d6b2694f54d03cb2bedce13d48b3095f
John <cooks.johns@gmail.com>
2018-05-07 21:47:54 -0500

    fixing files

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,14 @@
+package shapes;
+
+public class Rectangle {
+    pub int area;
+
+    public int pre = 2 x length + 2 x width;
+    pub int area = length * width;
+
+    public int getArea(){
+        return area;
+    }
+
+    pu
+}
