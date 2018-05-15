0d77f8175df44dca80a055459902f594198778b0
John <cooks.johns@gmail.com>
2018-05-15 00:46:34 -0500

    fixing up grades

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,11 +1,32 @@
 package grades;
 
+import java.util.Scanner;
 import java.util.HashMap;
 
 public class GradesApplication {
+
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         HashMap<String, Student> students = new HashMap<>();
         boolean cont;
 
+        students.put("JazzyJ", new Student("JazzyJ"));
+
+        students.get("JazzyJ").addGrade(99);
+        students.get("JazzyJ"). addGrade(99);
+
+        do {
+            Student JazzyJ = students.get("JazzyJ");;
+            System.out.printf("Welcome to the Grading app," + " \n" +
+                    "here are some of the student names: " + " \n" +
+            " JazzyJ ");
+
+            String choice2 = sc.next();
+            cont = choice2.equalsIgnoreCase("y");
+        } while (cont);
+
+    }
 
 }
