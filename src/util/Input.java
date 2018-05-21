1be93f7a2802db841bd2d1d6059cdfff062bf5d8
John <cooks.johns@gmail.com>
2018-05-21 16:44:19 -0500

    psvm was added to Student.java

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,57 +1,82 @@
 package util;
 import java.util.Scanner;
+import java.lang.NumberFormatException;
 
 public class Input {
 
 
+    private Scanner scanner;
 
-        private Scanner scanner;
+    public Input() {
 
-        public Input() {
+        scanner = new Scanner(System.in);
+    }
 
-            scanner = new Scanner(System.in);
+    public String getString() {
+
+        return scanner.nextLine();
+    }
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        return getString();
+
+    }
+
+
+    public boolean yesNo() {
+        String answer = scanner.nextLine();  // null
+        // auto-boxing => value (string) -> wrap it in a an object -> new String("y").equalsIgnoreCase()
+        return "y".equalsIgnoreCase(answer) || "yes".equalsIgnoreCase(answer);
+    }
+
+    public boolean yesNo(String prompt){
+        System.out.println(prompt);
+        return yesNo();
+    }
+
+
+    public int getInt(int min, int max) throws Exception {
+        int value = getInt();
+        if (value < min || value > max) {
+            System.out.printf("Enter a number between %d and %d%n", min, max);
+            return getInt(min, max);
         }
+        return value;
 
-        public String getString() {
+    }
 
-            return scanner.nextLine();
-        }
+    public int getInt() {
+        String input = getString();
 
+        try {
+            return Integer.valueOf(input);
 
-
-        public boolean yesNo() {
-            String answer = scanner.nextLine();  // null
-            // auto-boxing => value (string) -> wrap it in a an object -> new String("y").equalsIgnoreCase()
-            return "y".equalsIgnoreCase(answer) || "yes".equalsIgnoreCase(answer);
-        }
-
-
-        public int getInt(int min, int max) {
-            int value = getInt();
-            if (value < min || value > max) {
-                System.out.printf("Enter a number between %d and %d%n", min, max);
-                return getInt(min, max);
-            }
-            return value;
-        }
-        public int  getInt() {
-            Scanner scanner = new Scanner(System.in);
-            return scanner.nextInt();
-        }
-
-
-        public double getDouble(double min, double max) {
-            double value = getDouble();
-            if (value < min || value > max) {
-                System.out.printf("Enter a number between %f and %f%n", min, max);
-                return getDouble(min, max);
-            }
-            return value;
-        }
-        public double getDouble() {
-            double number = scanner.nextDouble();
-            scanner.nextLine();
-            return number;
+        } catch (NumberFormatException e) {
+            System.out.println("Must input an integer. ");
+            return getInt();
         }
     }
+
+
+    public double getDouble(double min, double max) {
+        System.out.printf("Enter a number between %f and %f%n", min, max);
+        double value = getDouble();
+        if (value < min || value > max) {
+            return getDouble(min, max);
+        }
+        return value;
+    }
+
+    public double getDouble() {
+        String input = getString();
+        try {
+            return Double.valueOf(input);
+        } catch (NumberFormatException e) {
+            System.out.println("Input must be a number.");
+            return getDouble();
+        }
+    }
+
+
+}
 
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,29 +3,32 @@ package util;
 public class InputTest {
     public static void main(String[] args) {
         Input input = new Input(); // instantiate an object / create an object
+//
+//        System.out.println("Enter a message");
+//        System.out.println(input.getString());
+//
+//        System.out.println("Do you want to continue (y/n)");
+//        System.out.println(input.yesNo());
+//
+//        System.out.println("Enter a number between 6 and 18");
+//        input.getInt(6, 18);
+//
+//        System.out.println("Enter any number");
+//        System.out.println(input.getInt());
+//
+//        int intNum = input.getInt();
+//        System.out.println(intNum);
+//
+//        double doubleInRange = input.getDouble(2.5, 7.9);
+//        System.out.println("Enter a number between 2.5 and 7.9");
+//        System.out.println(doubleInRange);
+//
+//        System.out.println("Enter any number");
+//        System.out.println(input.getDouble());
 
-        System.out.println("Enter a message");
-        System.out.println(input.getString());
+        int number = input.getInt();
 
-        System.out.println("Do you want to continue (y/n)");
-        System.out.println(input.yesNo());
-
-        System.out.println("Enter a number between 6 and 18");
-        input.getInt(6, 18);
-
-        System.out.println("Enter any number");
-        System.out.println(input.getInt());
-
-        int intNum = input.getInt();
-        System.out.println(intNum);
-
-        double doubleInRange = input.getDouble(2.5, 7.9);
-        System.out.println("Enter a number between 2.5 and 7.9");
-        System.out.println(doubleInRange);
-
-        System.out.println("Enter any number");
-        System.out.println(input.getDouble());
-
+        System.out.println(number);
 
     }
 }
